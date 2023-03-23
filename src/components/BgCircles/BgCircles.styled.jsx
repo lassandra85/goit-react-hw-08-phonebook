b4f8a7b158ce362fbe0beda115a902f4444b9eaf
lassandra85 <lassandra85@gmail.com>
2023-03-23 23:26:41 +0100

    add3

--- src/components/BgCircles/BgCircles.styled.jsx
+++ src/components/BgCircles/BgCircles.styled.jsx
@@ -40,72 +40,73 @@ export const CirclesItem = styled.li`
   color: rgba(47, 47, 47, 0.6);
   width: 20px;
   height: 20px;
+  padding: 3px;
  /*  #ebf5fc */
   list-style: none;
   background-color: rgba(255, 255, 255, 0.5);
   animation: ${bubblingAnimate} 25s linear infinite;
   &:nth-of-type(1) {
     left: 25%;
-    width: 80px;
-    height: 40px;
+    width: 86px;
+    height: 46px;
     animation-delay: 0s;
   }
   &:nth-of-type(2) {
     left: 10%;
-    width: 80px;
-    height: 40px;
+    width: 86px;
+    height: 46px;
     animation-delay: 2s;
     animation-duration: 12s;
   }
   &:nth-of-type(3) {
     left: 70%;
-    width: 40px;
-    height: 20px;
+    width: 46px;
+    height: 26px;
     animation-delay: 4s;
   }
   &:nth-of-type(4) {
     left: 40%;
-    width: 100px;
-    height: 80px;
+    width: 106px;
+    height: 86px;
     animation-delay: 0s;
     animation-duration: 18s;
   }
   &:nth-of-type(5) {
     left: 65%;
-    width: 70px;
-    height: 40px;
+    width: 76px;
+    height: 46px;
     animation-delay: 0s;
   }
   &:nth-of-type(6) {
     left: 75%;
-    width: 110px;
-    height: 20px;
+    width: 116px;
+    height: 26px;
     animation-delay: 3s;
   }
   &:nth-of-type(7) {
     left: 35%;
-    width: 150px;
-    height: 20px;
+    width: 146px;
+    height: 26px;
     animation-delay: 7s;
   }
   &:nth-of-type(8) {
     left: 50%;
-    width: 70px;
-    height: 40px;
+    width: 76px;
+    height: 46px;
     animation-delay: 15s;
     animation-duration: 45s;
   }
   &:nth-of-type(9) {
     left: 20%;
-    width: 30px;
-    height: 15px;
+    width: 36px;
+    height: 22px;
     animation-delay: 2s;
     animation-duration: 35s;
   }
   &:nth-of-type(10) {
     left: 85%;
-    width: 130px;
-    height: 20px;
+    width: 136px;
+    height: 26px;
     animation-delay: 0s;
     animation-duration: 11s;
   }
